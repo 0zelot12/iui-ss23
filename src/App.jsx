d60b1f0c73cc37e2aa0f33ef7d9a54650a8b4004
alauritz <andreas.lauritz@xavo.com>
2023-06-06 11:23:57 +0200

    Change name and add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import CameraInput from "./components/CameraInput";
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 export default function App() {
   return (
     <div className="App">
+      <Header></Header>
       <div className="container p-2 space-y-2 mx-auto flex flex-col items-center">
-        <h1 className="text-2xl">Sign Language Translator v0</h1>
         <CameraInput facingMode="environment" />
       </div>
       <Navbar></Navbar>
---
+++ src/components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+    return <div className="p-4 bg-blue-400 text-center"><h1 className="text-xl">Manolingo</h1></div>;
+}
+
+export default Header;
