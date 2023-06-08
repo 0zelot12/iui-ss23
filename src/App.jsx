18ddd8f15a26a3e21be7aef5761a97ec73e57bf2
Andreas Lauritz <lauritz.andreas@gmail.com>
2023-06-08 16:25:42 +0200

    Remove Navbar for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import CameraInput from "./components/CameraInput";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
@@ -7,7 +6,6 @@ function App() {
       <div className="container p-2 space-y-2 mx-auto flex flex-col items-center">
         <CameraInput facingMode="environment" />
       </div>
-      <Navbar></Navbar>
     </div>
   );
 }
