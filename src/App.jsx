f6bcf8a09d5e0dfef75df7bbd6688ca76d6db66a
alauritz <lauritz.andreas@gmail.com>
2023-06-09 11:07:08 +0200

    Chage colours

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ function App() {
             Capture
           </button>
           <button
-            className="bg-blue-500 px-4 py-2 rounded text-white"
+            className="bg-blue-800 px-4 py-2 rounded text-white"
             onClick={onTranslate}
           >
             Translate
