e34b376b4f685b69bb9c796f0ce785418c30b823
Andreas Lauritz <lauritz.andreas@gmail.com>
2023-06-20 23:35:57 +0200

    Minor adjustment

--- src/components/PreviewBox.jsx
+++ src/components/PreviewBox.jsx
@@ -2,7 +2,7 @@ import { PreviewItem } from "./PreviewItem";
 
 function PreviewBox({ items }) {
   return items.length === 0 ? null : (
-    <ol className="border-2 border-blue-500 flex p-2 justify-start space-x-2 rounded">
+    <ol className="border-2 border-blue-500 flex flex-wrap p-2 justify-start space-x-2 rounded gap-y-2">
       {items.map((item) => (
         <PreviewItem item={item} />
       ))}
