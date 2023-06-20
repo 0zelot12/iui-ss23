ae3842eba376e693e1d8683c54d25f55df7793f0
Andreas Lauritz <lauritz.andreas@gmail.com>
2023-06-20 22:54:15 +0200

    Center spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -4,7 +4,7 @@ function Spinner({ active }) {
   } else {
     return (
       <svg
-        class="animate-spin -ml-1 mr-3 h-5 w-5 text-blue-500"
+        class="animate-spin h-5 w-5 text-blue-500"
         xmlns="http://www.w3.org/2000/svg"
         fill="none"
         viewBox="0 0 24 24"
