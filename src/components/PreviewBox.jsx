a92cf5d98d532459a7f3b4c78d82b3b4e2709dd2
alauritz <andreas.lauritz@xavo.com>
2023-07-05 11:24:11 +0200

    Minor design changes

--- src/components/PreviewBox.jsx
+++ src/components/PreviewBox.jsx
@@ -2,7 +2,7 @@ import { PreviewItem } from "./PreviewItem";
 
 function PreviewBox({ items, onItemClick }) {
   return items.length === 0 ? null : (
-    <ol className="border-2 border-blue-500 flex flex-wrap p-2 justify-start space-x-2 rounded gap-y-2">
+    <ol className="border-2 border-blue-500 flex flex-wrap p-2 justify-start space-x-2 rounded-xl gap-y-2">
       {items.map((item) => (
         <PreviewItem key={item.id} item={item} onClick={onItemClick} />
       ))}
--- src/components/PreviewItem.jsx
+++ src/components/PreviewItem.jsx
@@ -1,7 +1,7 @@
 function PreviewItem({ item, onClick }) {
   return (
     <li
-      className="p-2 border-2 border-blue-500 rounded text-blue-950 cursor-pointer"
+      className="py-2 px-4 bg-blue-200 rounded-lg shadow text-blue-950 cursor-pointer"
       onClick={() => {
         onClick(item);
       }}
